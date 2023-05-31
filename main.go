b9b34d9522eadd5bd6fdd368e06164748161edfb
William Okuno <38401861+wokuno@users.noreply.github.com>
2023-05-31 02:59:16 -0500

    oops

--- main.go
+++ main.go
@@ -67,16 +67,17 @@ func main() {
 
 }
 
+// Middleware to add CORS headers
 func addCORSHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Add CORS headers
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Vary", "Origin")
 
 		// Handle pre-flight OPTIONS request
 		if r.Method == http.MethodOptions {
-			w.WriteHeader(http.StatusOK)
 			return
 		}
 
